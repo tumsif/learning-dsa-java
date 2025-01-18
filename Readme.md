46108e73abeb2ff85920830e3221182e5abff810
Tumsifu Lema <tom455976@gmail.com>
2025-01-18 23:03:50 +0300

    updated readme and created LinkedList java file

--- Readme.md
+++ Readme.md
@@ -17,5 +17,30 @@ But make sure you allow the excecutable permissions to this two using command
 
 `sudo chmod u+x compile run`
 
-## Commented Code
-Some code appears to be commented because of reused variables
+## Algorithm and Data Structure
+- An algorithm is a high level, language independent description of a step by step process of performing a task.
+
+- A Data structure is a specific family of algorithms for implementing an abstract data type.
+
+- Abstract data type is a mathematical description of an object with set of operations on the object.
+
+### Abstract Data type
+Is a data declaration packaged together with operations that are meaningful for the data type.
+
+It encapsulates the data and operations on the data, and then hidden from the user.
+
+Since it uses abstraction under the hood.
+This means
+- We know what a data type can do.
+- How it is done is hidden for the user
+
+With an ADT users are not concerned with how a task is done but rather with what it can do.
+
+Examples of ADT
+- Lists [see more](./tz/ac/udsm/dsa/lists/about.md)
+- Stacks [see more](./tz/ac/udsm/dsa/stacks/about.md)
+- Queues [see more](./tz/ac/udsm/dsa/queues/about.md)
+- Trees
+- Heaps
+- Graphs
+
---
+++ tz/ac/udsm/dsa/lists/LinkedList.java
@@ -0,0 +1,82 @@
+package tz.ac.udsm.dsa.lists;
+/**
+  * A linked list is data structure, in which a node consist of data and the address of the next the node successor.
+  * This in turn can have a variable length of collection compared to arrays
+  */
+class LinkedList<T> {
+    LinkedList next;
+    T value;
+
+    LinkedList(T value, LinkedList next){
+        this.value = value;
+        this.next = next;
+    }
+
+    public void printAllElements(){
+        /**
+         * Accessing all elements
+         */
+        LinkedList node = this;
+        while(node != null){
+            System.out.println(node.value + ", ");
+            node = node.next;
+        }
+    }
+
+    /**
+     * Inserting a node into a SSL (Singly Linked List)
+     * 
+     * As a new first element
+     * As the new last element
+     * Before a given node (specifies by reference)
+     * After a given node
+     * Before a given value
+     * After a given value
+     */
+
+    // Insert at first location
+    public LinkedList insertAtFirst(Object value){
+        LinkedList newNode = new LinkedList(value, this);
+        return newNode;
+    }
+
+    // insert a node after a given value
+    public void insertAfterValue(Object beforeValue, Object afterValue){
+        LinkedList node = this;
+        while(node != null){
+            if(node.value.equals(beforeValue)){
+                LinkedList newNode = new LinkedList(afterValue, node.next);
+                node.next = newNode;
+                return;
+            }
+            node = node.next;
+        }
+        System.out.println("Value " + beforeValue + " not found. No changes made!");
+    }
+
+    public void deleteValue(Object value){
+        LinkedList currentNode = this;
+        LinkedList previousNode = null;
+
+        while(currentNode != null){
+            if(currentNode.value.equals(value)){
+                if(previousNode == null){
+                    // this means it is the first element
+                    // thus updating the link to point to the next element deletes the node
+                    currentNode = currentNode.next;
+                    return;
+                }else{
+                    // this means it is not the first element, and therefore updating the previous link to point to next elment
+                    previousNode.next = currentNode.next;
+                    return;
+                }
+            }
+
+            // moving to next node
+            previousNode = currentNode;
+            currentNode = currentNode.next;
+        }
+
+        System.out.println("There is no value " + value);
+    }
+}
