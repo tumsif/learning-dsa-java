51f41764d4a9617acee751b5758c8ca5b74de317
Tumsifu Lema <tom455976@gmail.com>
2025-01-18 14:06:30 +0300

    modified readme and updated the Arrays and RaggedArray java files

--- Readme.md
+++ Readme.md
@@ -1,3 +1,19 @@
-# learning dsa
-Run ./compile to convert your application to jar file
-Tell more about your project
+# Learning DSA (Data structure and algorithm)
+
+Hi, 😊 I am Tumsifu Lema. Welcome to my repository
+
+This repository has some of what i learnt when studing Data structures and algorithm.
+It doesnt cover much but since you visited this repository have a look.
+
+## To compile
+*(Tested on Linux) - I dont know about you Windows folks.* 🤣
+
+`./compile` to convert your application to jar file
+
+## To Run the script
+`./run` runs the script
+
+But make sure you allow the excecutable permissions to this two using command
+
+`sudo chmod u+x compile run`
+
--- tz/ac/udsm/dsa/Main.java
+++ tz/ac/udsm/dsa/Main.java
@@ -2,8 +2,8 @@ package tz.ac.udsm.dsa;
 
 import tz.ac.udsm.dsa.Ackermann;
 import tz.ac.udsm.dsa.Factorial;
-import tz.ac.udsm.dsa.Arrays;
-import tz.ac.udsm.dsa.RaggedArray;
+import tz.ac.udsm.dsa.arrays.Arrays;
+import tz.ac.udsm.dsa.arrays.RaggedArray;
 import tz.ac.udsm.dsa.Node;
 
 import tz.ac.udsm.dsa.recursion.Palindrome;
@@ -17,70 +17,70 @@ import tz.ac.udsm.dsa.iterations.Exponential;
 
 class Main {
     public static void main(String[] args){
-        // Ackermann ackerman = new Ackermann();
-        // ackerman.run();
+        Ackermann ackerman = new Ackermann();
+        ackerman.run();
 
-        // Factorial factorial = new Factorial();
-        // factorial.run();
+        Factorial factorial = new Factorial();
+        factorial.run();
 
-        // Arrays arr = new Arrays();
-        // System.out.println(arr.matrix2[1][2]);
+        Arrays arr = new Arrays();
+        System.out.println(arr.matrix2[1][2]);
 
-        // int raggedArr[][] = {
-        //     {1, 2, 3, 4},
-        //     {5, 6, 7},
-        //     {8, 9},
-        //     {10}
-        // };
+        int raggedArr[][] = {
+            {1, 2, 3, 4},
+            {5, 6, 7},
+            {8, 9},
+            {10}
+        };
 
-        // int nonRaggedArr[][] = {
-        //     {1, 2, 3, 4},
-        //     {5, 6, 7, 8},
-        //     {9, 10, 11, 12},
-        //     {10, 11, 12, 13}
-        // };
+        int nonRaggedArr[][] = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12},
+            {10, 11, 12, 13}
+        };
 
-        // RaggedArray rgdArr = new RaggedArray();
-        // // System.out.println(rgdArr.isRagged(raggedArr));
-        // // System.out.println(rgdArr.isRagged(nonRaggedArr));
+        RaggedArray rgdArr = new RaggedArray();
+        // System.out.println(rgdArr.isRagged(raggedArr));
+        // System.out.println(rgdArr.isRagged(nonRaggedArr));
 
 
-        // Arrays arr = new Arrays();
+        Arrays arr = new Arrays();
 
-        // int arr1[][] = {
-        //     {10, 20, 30},
-        //     {50, 40}
-        // };
-        // int arr2[][] = {
-        //     {10, 20, 30},
-        //     {50, 40}
-        // };
-        // int arr3[][] = {
-        //     {10, 30, 20},
-        //     {50, 40, 60}
-        // };
-        // int arr4[][] = {
-        //     {10, 20, 30},
-        //     {40, 50, 60}
-        // };
-        // int arr5[][] = {
-        //     {10, 20, 30},
-        //     {40, 50},
-        //     {60}
-        // };
-        // int arr6[][] = {
-        //     {10, 20, 30},
-        //     {40, 50, 60}
-        // };
-        // // System.out.println(arr.equals(raggedArr, nonRaggedArr));
-        // System.out.println(arr.equals(arr1, arr2));
-        // System.out.println(arr.equals(arr3, arr4));
-        // System.out.println(arr.equals(arr5, arr6));
+        int arr1[][] = {
+            {10, 20, 30},
+            {50, 40}
+        };
+        int arr2[][] = {
+            {10, 20, 30},
+            {50, 40}
+        };
+        int arr3[][] = {
+            {10, 30, 20},
+            {50, 40, 60}
+        };
+        int arr4[][] = {
+            {10, 20, 30},
+            {40, 50, 60}
+        };
+        int arr5[][] = {
+            {10, 20, 30},
+            {40, 50},
+            {60}
+        };
+        int arr6[][] = {
+            {10, 20, 30},
+            {40, 50, 60}
+        };
+        // System.out.println(arr.equals(raggedArr, nonRaggedArr));
+        System.out.println(arr.equals(arr1, arr2));
+        System.out.println(arr.equals(arr3, arr4));
+        System.out.println(arr.equals(arr5, arr6));
 
-        // int[] linearizedArr = arr.linearize(arr1);
-        // for(int i=0; i<linearizedArr.length; i++){
-        //     System.out.println(arr.linearize(arr1)[i]);
-        // }
+        int[] linearizedArr = arr.linearize(arr1);
+        for(int i=0; i<linearizedArr.length; i++){
+            System.out.println(arr.linearize(arr1)[i]);
+        }
 
         // Node numerals = new Node<String>("one", new Node<String>("two", new Node<String>("three", null)));
         Node<Integer> numerals = new Node<Integer>(1, new Node<Integer>(2, new Node<Integer>(3, null)));
@@ -103,8 +103,8 @@ class Main {
         String word2 = "ono";
 
         Palindrome plndr = new Palindrome();
-        // System.out.println(plndr.isPalindrome(word, 0, (word.length() - 1)));
-        // System.out.println(plndr.isPalindrome(word2, 0, (word.length() - 1)));
+        System.out.println(plndr.isPalindrome(word, 0, (word.length() - 1)));
+        System.out.println(plndr.isPalindrome(word2, 0, (word.length() - 1)));
 
         // fibonacci numbers
         Fibonacci fib = new Fibonacci();
